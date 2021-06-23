a4f249a721c82f2b88efc6e00863f18355cf0a7a
DianaBRA <85389615+DianaBRA@users.noreply.github.com>
2021-06-22 19:48:45 -0500

    Create ejercicio4.py
    Creación  del codigo ejercicio 4

---
+++ ejercicio4.py
@@ -0,0 +1,14 @@
+from fastapi  import  FasAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI(title = "FastAPI con Jinja2")
+
+app.mount("/recursos", StaticFiles(directory="recursos"), name="recursos")
+
+miPlantilla = Jinja2Templates(directory="plantillas")
+
+@app.get("/inicio", response_class=HTMLResponse)
+async def read_item(request: Request):
+	return miPlantilla.TemplateResponse("index.html",{"request":request})
