6dccd9e436c563637f3b8ba9da3fb5dc9d0b4c00
DianaBRA <85389615+DianaBRA@users.noreply.github.com>
2021-06-24 13:33:23 -0500

    Create HTMLResponse.py

---
+++ ejercicios_Jinja2/HTMLResponse.py
@@ -0,0 +1,20 @@
+from fastapi  import  FasAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI ()
+
+@app.get("/", response_class=HTMLResponse)
+async def mostrar_inicio() :
+	 contenido_html = """
+	 <html>
+	 <head>
+	     <title>EquipoX</title>
+	  </head>
+	  <body>
+	      <h3>Bienvenidos</h3>
+	      <p>Este sitio pertenece al EquipoX y mostrara los datos de los integrantes</p>
+	      <a href="wilber.html"> wilber </a>
+	   </body>
+	   </html>
+	   """
+	   return HTMLResponse(content=contenido_html, Status_code=200)
